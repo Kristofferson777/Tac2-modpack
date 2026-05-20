6cd43022a16f2621651102ae2db3d3ce61a0b3d4
Christopher Hollhumer <113223059+Kristofferson777@users.noreply.github.com>
2026-05-20 02:34:05 -0400

    Add files via upload
    added auxiliary files other than addon folder

---
+++ mod.cpp
@@ -0,0 +1,11 @@
+name         = "Tac2 modpack";
+picture      = "TAC2oldlogo.paa";
+logoSmall    = "TAC2oldlogo.paa";
+logo         = "TAC2oldlogo.paa";
+logoOver     = "TAC2oldlogo.paa";
+action       = "";
+tooltipOwned = "Tac2 Unit Modpack Collection";
+
+overview     = "Tac2 Modpack for modlist consolidation and to add in assets and tweaks";
+hideName     = 0;
+hidePicture  = 0;
